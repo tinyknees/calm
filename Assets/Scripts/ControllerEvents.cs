1a0b8b7e6ac1cbda535c492830e4ec7685efe211
Kevin Cheng <kc@kevnull.com>
2016-11-14 18:58:58 -0800

    Basic Laser Pointer Events
    Added some laser pointer stuff. Shows green until it collides with an
    object and turns red. No actions but has event handlers for trigger and
    hits.

---
+++ Assets/Scripts/ControllerEvents.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Defines and Publishes Vive controller events
+/// Add this as a component to each controller for which you'd like to listen to events
+/// </summary>
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ControllerEvents : MonoBehaviour
+{
+
+    // Event Declaration Code
+    public struct ControllerInteractionEventArgs
+    {
+        public uint controllerIndex;
+    }
+
+    public delegate void ControllerInteractionEventHandler(object sender, ControllerInteractionEventArgs e);
+
+    [System.Serializable]
+    public class ControllerEvent : UnityEvent<ControllerInteractionEventArgs> { }
+
+    // Native C# Events are more efficient but can only be used from code
+    public event ControllerInteractionEventHandler TriggerPressed;
+    public event ControllerInteractionEventHandler TriggerReleased;
+
+    // Unity Events add a little overhead but listeners can be assigned via Unity editor
+    public ControllerEvent unityTriggerPressed = new ControllerEvent();
+    public ControllerEvent unityTriggerReleased = new ControllerEvent();
+
+    // Member Variables
+    [HideInInspector]
+    public bool triggerPressed = false;
+
+    private uint controllerIndex;
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+
+    //Unity lifecycle method
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    //Unity lifecycle method
+    private void Update()
+    {
+        controllerIndex = (uint)trackedObj.index;
+        device = SteamVR_Controller.Input((int)controllerIndex);
+
+        //Trigger Pressed
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            OnTriggerPressed(SetButtonEvent(ref triggerPressed, true));
+        }
+        else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            OnTriggerReleased(SetButtonEvent(ref triggerPressed, false));
+        }
+    }
+
+    // Creates, fills out, and returns a new ControllerInteractionEventArgs struct
+    // (Convenience method to reduce code duplication)
+    private ControllerInteractionEventArgs SetButtonEvent(ref bool buttonBool, bool value)
+    {
+        buttonBool = value;
+        ControllerInteractionEventArgs e;
+        e.controllerIndex = controllerIndex;
+        return e;
+    }
+
+    // Event publisher
+    public virtual void OnTriggerPressed(ControllerInteractionEventArgs e)
+    {
+        if (TriggerPressed != null)
+        {
+            TriggerPressed(this, e);
+        }
+        unityTriggerPressed.Invoke(e);
+    }
+
+    // Event publisher
+    public virtual void OnTriggerReleased(ControllerInteractionEventArgs e)
+    {
+        if (TriggerReleased != null)
+        {
+            TriggerReleased(this, e);
+        }
+        unityTriggerReleased.Invoke(e);
+    }
+
+}
---
+++ Assets/Scripts/PaintObject.cs
@@ -0,0 +1,242 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// </summary>
+[RequireComponent(typeof(ControllerEvents))]
+[RequireComponent(typeof(LaserPointer))]
+public class PaintObject : MonoBehaviour
+{
+
+    // Moving Object Stuff
+    private LaserPointer laserPointer;
+    private ControllerEvents controllerEvents;
+    private LaserPointer.PointerEventArgs hitObj;
+    private ControllerEvents.ControllerInteractionEventArgs activeController;
+    private Color laserPointerDefaultColor;
+
+    private Vector3 lastControllerPos;
+
+    private bool movingObject = false;
+    private bool triggerPressed = false;
+    private bool hitTarget = false;
+    private bool drawing;
+
+
+    //paint
+    public GameObject brushCursor, brushContainer; //The cursor that overlaps the model and our container for the brushes painted
+    public Camera sceneCamera, canvasCam;  //The camera that looks at the model, and the camera that looks at the canvas.
+    public Sprite cursorPaint, cursorDecal; // Cursor for the differen functions 
+    public RenderTexture canvasTexture; // Render Texture that looks at our Base Texture and the painted brushes
+    public Material baseMaterial; // The material of our base texture (Were we will save the painted texture)
+    public SteamVR_TrackedController controller;
+
+    float brushSize = 1.0f; //The size of our brush
+    Color brushColor; //The selected color
+    int brushCounter = 0, MAX_BRUSH_COUNT = 1000; //To avoid having millions of brushes
+    bool saving = false; //Flag to check if we are saving the texture
+
+
+
+    // Unity lifecycle method
+    void Awake()
+    {
+        laserPointer = GetComponent<LaserPointer>();
+        controllerEvents = GetComponent<ControllerEvents>();
+
+        laserPointerDefaultColor = laserPointer.color;
+    }
+
+    void Init()
+    {
+    }
+
+    // Unity lifecycle method
+    void OnEnable()
+    {
+        controllerEvents.TriggerPressed += HandleTriggerPressed;
+        controllerEvents.TriggerReleased += HandlerTriggerReleased;
+        laserPointer.PointerIn += HandlePointerIn;
+        laserPointer.PointerOut += HandlePointerOut;
+    }
+
+    // Unity lifecycle method
+    void OnDisable()
+    {
+        controllerEvents.TriggerPressed -= HandleTriggerPressed;
+        controllerEvents.TriggerReleased -= HandlerTriggerReleased;
+        laserPointer.PointerIn -= HandlePointerIn;
+        laserPointer.PointerOut -= HandlePointerOut;
+    }
+
+    // Unity lifecycle method
+    void Update()
+    {
+//        UpdateBrushCursor();
+        if (hitTarget && triggerPressed)
+        {
+            drawing = true;
+            DoAction();
+        }
+        else
+        {
+            drawing = false;
+        }
+
+    }
+
+    //Event Handler
+    private void HandleTriggerPressed(object sender, ControllerEvents.ControllerInteractionEventArgs e)
+    {
+        lastControllerPos = controllerEvents.gameObject.transform.position;
+        laserPointer.enabled = true;
+        triggerPressed = true;
+        laserPointer.PointerIn -= HandlePointerIn;
+        laserPointer.PointerOut -= HandlePointerOut;
+    }
+
+    //Event Handler
+    private void HandlerTriggerReleased(object sender, ControllerEvents.ControllerInteractionEventArgs e)
+    {
+        //laserPointer.enabled = false;
+        triggerPressed = false;
+        movingObject = false;
+        laserPointer.PointerIn += HandlePointerIn;
+        laserPointer.PointerOut += HandlePointerOut;
+    }
+
+    //Event Handler
+    private void HandlePointerIn(object sender, LaserPointer.PointerEventArgs e)
+    {
+        laserPointer.pointerModel.GetComponent<MeshRenderer>().material.color = Color.red;
+        hitTarget = true;
+        hitObj = e;
+        laserPointer.PointerUpdate += HandlePointerUpdate;
+    }
+
+    //Event Handler
+    private void HandlePointerOut(object sender, LaserPointer.PointerEventArgs e)
+    {
+        laserPointer.pointerModel.GetComponent<MeshRenderer>().material.color = laserPointerDefaultColor;
+        hitTarget = false;
+        laserPointer.PointerUpdate -= HandlePointerUpdate;
+    }
+
+    private void HandlePointerUpdate(object sender, LaserPointer.PointerEventArgs e)
+    {
+        hitObj = e;
+    }
+
+    void DoAction()
+    {
+        if (saving)
+            return;
+        Vector3 uvWorldPosition = Vector3.zero;
+        if (HitTestUVPosition(ref uvWorldPosition))
+        {
+            GameObject brushObj;
+
+            brushObj = (GameObject)Instantiate(Resources.Load("TexturePainter-Instances/BrushEntity")); //Paint a brush
+            brushObj.GetComponent<SpriteRenderer>().color = brushColor; //Set the brush color
+
+            brushColor.a = brushSize * 2.0f; // Brushes have alpha to have a merging effect when painted over.
+            brushObj.transform.parent = brushContainer.transform; //Add the brush to our container to be wiped later
+            brushObj.transform.localPosition = uvWorldPosition; //The position of the brush (in the UVMap)
+            brushObj.transform.localScale = Vector3.one * brushSize;//The size of the brush
+        }
+        brushCounter++; //Add to the max brushes
+        if (brushCounter >= MAX_BRUSH_COUNT)
+        { //If we reach the max brushes available, flatten the texture and clear the brushes
+            brushCursor.SetActive(false);
+            saving = true;
+            Invoke("SaveTexture", 0.1f);
+
+        }
+    }
+    //To update at realtime the painting cursor on the mesh
+    void UpdateBrushCursor()
+    {
+        Vector3 uvWorldPosition = Vector3.zero;
+        if (HitTestUVPosition(ref uvWorldPosition) && !saving)
+        {
+            brushCursor.SetActive(true);
+            brushCursor.transform.position = uvWorldPosition + brushContainer.transform.position;
+        }
+        else
+        {
+            brushCursor.SetActive(false);
+        }
+    }
+    //Returns the position on the texuremap according to a hit in the mesh collider
+    bool HitTestUVPosition(ref Vector3 uvWorldPosition)
+    {
+        RaycastHit hit;
+        Ray cursorRay = new Ray(sceneCamera.transform.position, sceneCamera.transform.forward);
+        if (Physics.Raycast(cursorRay, out hit, 200))
+        {
+            MeshCollider meshCollider = hitObj.target.GetComponent<Collider>() as MeshCollider;
+            if (meshCollider == null || meshCollider.sharedMesh == null)
+                return false;
+            Vector2 pixelUV = new Vector2(hitObj.textureCoord2.x, hitObj.textureCoord2.y);
+            uvWorldPosition.x = pixelUV.x - canvasCam.orthographicSize;//To center the UV on X
+            uvWorldPosition.y = pixelUV.y - canvasCam.orthographicSize;//To center the UV on Y
+            uvWorldPosition.z = 0.0f;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    //Sets the base material with a our canvas texture, then removes all our brushes
+    void SaveTexture()
+    {
+        brushCounter = 0;
+        System.DateTime date = System.DateTime.Now;
+        RenderTexture.active = canvasTexture;
+        Texture2D tex = new Texture2D(canvasTexture.width, canvasTexture.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, canvasTexture.width, canvasTexture.height), 0, 0);
+        tex.Apply();
+        RenderTexture.active = null;
+        baseMaterial.mainTexture = tex; //Put the painted texture as the base
+        foreach (Transform child in brushContainer.transform)
+        {//Clear brushes
+            Destroy(child.gameObject);
+        }
+        //StartCoroutine ("SaveTextureToFile"); //Do you want to save the texture? This is your method!
+        Invoke("ShowCursor", 0.1f);
+    }
+    //Show again the user cursor (To avoid saving it to the texture)
+    void ShowCursor()
+    {
+        saving = false;
+    }
+
+    ////////////////// PUBLIC METHODS //////////////////
+    public void SetBrushSize(float newBrushSize)
+    { //Sets the size of the cursor brush or decal
+        brushSize = newBrushSize;
+        brushCursor.transform.localScale = Vector3.one * brushSize;
+    }
+
+    ////////////////// OPTIONAL METHODS //////////////////
+
+#if !UNITY_WEBPLAYER
+    IEnumerator SaveTextureToFile(Texture2D savedTexture)
+    {
+        brushCounter = 0;
+        string fullPath = System.IO.Directory.GetCurrentDirectory() + "\\UserCanvas\\";
+        System.DateTime date = System.DateTime.Now;
+        string fileName = "CanvasTexture.png";
+        if (!System.IO.Directory.Exists(fullPath))
+            System.IO.Directory.CreateDirectory(fullPath);
+        var bytes = savedTexture.EncodeToPNG();
+        System.IO.File.WriteAllBytes(fullPath + fileName, bytes);
+        Debug.Log("<color=orange>Saved Successfully!</color>" + fullPath + fileName);
+        yield return null;
+    }
+#endif
+}
+
