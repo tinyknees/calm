b54daabbe18f6b1a2d1a1faec3df8e54d058cbef
Kevin Cheng <kc@kevnull.com>
2017-02-03 14:09:03 -0800

    Trigger grip animation on trigger press

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class HandAnimator : MonoBehaviour
+{
+
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        myAnimator = VRTK_DeviceFinder.GetControllerLeftHand(true).GetComponentInChildren<Animator>();
+        GetComponent<VRTK_ControllerEvents>().TriggerTouchStart += new ControllerInteractionEventHandler(DoTriggerTouchStart);
+        GetComponent<VRTK_ControllerEvents>().TriggerTouchEnd += new ControllerInteractionEventHandler(DoTriggerTouchEnd);
+    }
+
+    private void DoTriggerTouchStart(object sender, ControllerInteractionEventArgs e)
+    {
+        myAnimator.SetBool("Grab", true);
+    }
+
+    private void DoTriggerTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        myAnimator.SetBool("Grab", false);
+    }
+}
